ce235fe2194e8817b646d58c0f830d43a873338a
lucass <24lspeier@athenian.org>
2021-09-22 10:19:28 -0700

    Nathan

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -11,8 +11,9 @@ public class MyMain {
     //    int[] arr2 = {2, 4, 6};
     //    int[] arr3 = addArrays3(arr1, arr2); // arr3 = [3, 7, 11]
     public static int[] addArrays3(int[] arr1, int[] arr2) {
+
         // REPLACE THIS WITH YOUR CODE
-        return null;
+        return new int[] {arr1[0]+arr2[0],arr1[1]+arr2[1],arr1[2]+arr2[2]};
     }
 
     // Arrays Basics Problem 2:
@@ -24,7 +25,8 @@ public class MyMain {
     // double[] arr2 = average3(arr1); // arr2 = [3.0, 3.0, 3.0]
     public static double[] average3(int[] arr) {
         // REPLACE THIS WITH YOUR CODE
-        return null;
+        double avg = (0.0+arr[0]+arr[1]+arr[2])/3.0;
+        return new double[] {avg,avg,avg};
     }
 
     // Iteration Problem 1:
@@ -34,8 +36,12 @@ public class MyMain {
     // int[] nums = {1, 6, 2, 6, 8, 2, 3, 9, 3};
     // int x = sum(nums); // x = 40
     public static int sum(int[] arr) {
+        int total = 0;
+        for (int x: arr) {
+            total = total + x;
+        }
         // REPLACE THIS WITH YOUR CODE
-        return -1;
+        return total;
     }
 
     // Iteration Problem 2:
@@ -45,8 +51,14 @@ public class MyMain {
     // int[] nums = {1, 6, 2, 6, 8, 2, 3, 9, 3};
     // int x = max(nums); // x = 9
     public static int max(int[] arr) {
+        int biggest = -9999999;
+        for (int x: arr) {
+            if (x > biggest) {
+                biggest = x;
+            }
+        }
         // REPLACE THIS WITH YOUR CODE
-        return -1;
+        return biggest;
     }
 
     // Challenge Problem:
@@ -58,7 +70,11 @@ public class MyMain {
     // makeRandomArray(10, 1, 5) => [4, 2, 3, 1, 5, 3, 4, 4, 1, 2]
     public static int[] makeRandomArray(int size, int lowest, int biggest) {
         // REPLACE THIS WITH YOUR CODE
-        return null;
+        int[] arr = new int[size];
+        for (int x = 0; x<size; x++) {
+            arr[x] = (int) (Math.random()*(biggest-lowest) +lowest);
+        }
+        return arr;
     }
 
 
